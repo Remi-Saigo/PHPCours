3fd39f59c3900056457fddae4fc9b7aceb438485
Remi <legrandsoleil62@gmail.com>
2025-10-21 13:46:15 +0200

    first commit

---
+++ contact.php
@@ -0,0 +1,5 @@
+<h1 class = "text-danger text-center">Contact</h1>
+<h2 class = "text-dark text-center">adressemaildecontact@gmail.com</h2>
+<h2 class = "text-dark text-center">12 rue des Lilas </h2>
+<p>Dijon</p>
+<h2 class = "text-dark text-center">Remouille Proyouille</h2>
