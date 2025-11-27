94352a529877bfa3bace02dc5174f56279a69cb1
Remi-Saigo <legrandsoleil62@gmail.com>
2025-11-27 17:30:08 +0100

    Test Suppression

--- ListeUser.php
+++ ListeUser.php
@@ -13,8 +13,21 @@ if (isset($_SESSION['login'])) {
             $sql->bindParam(':id', $_GET['user'], PDO::PARAM_INT);
             $r = $sql->execute();
         }
+        if ((isset($_GET['action'])) && (isset($_GET['user']))) {
+            if ($_GET['action'] == 'supprimmer') {
+                $r = 'User';
+            } else {
+                $r = 'Admin';
+            }
+            $sql = $dbh->prepare("UPDATE User set role= :role where id  = :id");
+            $sql->bindParam(':role', $r, PDO::PARAM_STR);
+            $sql->bindParam(':id', $_GET['user'], PDO::PARAM_INT);
+            $r = $sql->execute();
+        }
+        
+
         $sql = 'SELECT name, surname, email,role,id FROM User';
-        echo "<table> <tr> <th>Nom</th> <th>Prénom</th> <th>Email</th> <th>Rôle</th></tr>";
+        echo "<table> <tr> <th>Nom</th> <th>Prénom</th> <th>Email</th> <th>Rôle</th> <th>Delete</th></tr>";
         foreach ($dbh->query($sql) as $row) {
             echo "<tr><td>";
             echo $row['name'] . "\t";
@@ -24,7 +37,7 @@ if (isset($_SESSION['login'])) {
             echo $row['email'] . "\t";
             echo "</td><td> <a href=\"index.php?page=ListeUser&user=" . $row['id'] . "&role=" . $row['role'] . "\">";
             echo $row['role'] . "\t";
-            echo "</a></td></tr>";
+            echo "</a></td><td><a href=\"index.php?page=ListeUser&user=" . $row['id'] . "&action=supprimmer\"> Supprimer</a></td></tr>";
         }
         echo "</table>";} else {
         echo 'Vous n\'avez pas les permsssions administrateur<br>';
